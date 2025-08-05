6505dce1b4805e3bc1fc73acffa679a1e0deced4
PIV0097 <PIV0097@yandex.ru>
2025-08-05 17:09:13 +0400

    HW2 ready full

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -21,42 +21,41 @@ public class ArrayStorage {
         if (r == null)
             throw new IllegalArgumentException("Resume не может быть null");
 
-        for (int i = 0; i < size; i++) {
-            if (r.getUuid().equals(storage[i].getUuid())) {
-                storage[i] = r;
-                return;
-            }
-        }
-        System.out.println("Резюме не обновлено, отсутствует в базе данных");
+        int index = getIndex(r.getUuid());
+
+        if (index == -1)
+            System.out.println("Резюме не обновлено, отсутствует в базе данных");
+
+        storage[index] = r;
     }
 
     public void save(Resume r) {
         if (r == null)
             throw new IllegalArgumentException("Resume не может быть null");
+
         if (size >= storage.length) {
             throw new IllegalArgumentException("Хранилище переполнено");
         }
-        for (int i = 0; i < size; i++) {
-            if (r.getUuid().equals(storage[i].getUuid())) {
-                throw new IllegalArgumentException("Резюме с uuid" + r.getUuid() + " уже есть в базе данных");
-            }
+
+        if (getIndex(r.getUuid()) == -1) {
+            storage[size] = r;
+            size++;
+        } else {
+            throw new IllegalArgumentException("Резюме с uuid" + r.getUuid() + " уже есть в базе данных");
         }
-        storage[size] = r;
-        size++;
     }
 
     public Resume get(String uuid) {
         if (uuid == null) {
             throw new IllegalArgumentException("UUID не может быть null");
         }
+        int index = getIndex(uuid);
+        if (index != -1)
+            return storage[index];
 
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
-        }
         System.out.println("Резюме с uuid " + uuid + " нет в базе данных");
         return null;
+
     }
 
     public void delete(String uuid) {
@@ -64,15 +63,14 @@ public class ArrayStorage {
             throw new IllegalArgumentException("UUID не может быть пустым");
         }
 
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                return;
-            }
+        int index = getIndex(uuid);
+        if (index == -1)
+            throw new IllegalArgumentException("Резюме с UUID '" + uuid + "' не найдено");
+        else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-        throw new IllegalArgumentException("Резюме с UUID '" + uuid + "' не найдено");
     }
 
     public Resume[] getAll() {
@@ -81,6 +79,20 @@ public class ArrayStorage {
 
     public int size() {
         return size;
+    }
+
+    /**
+     * Возвращает индекс найденного элемента, будет исвользоваться в методах класса для исключения дублирования кода
+     * <p>
+     * Принимает uuid, при помощи цикла проходит по каждому элементу storage,
+     * если uuid совпадают, возвращает индекс, если нет -1
+     */
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid))
+                return i;
+        }
+        return -1;
     }
 
 }
